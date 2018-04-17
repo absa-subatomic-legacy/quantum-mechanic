2d869720d771bff737061a4d4a1364208cc077d2
KieranHons <kieran.bester@gmail.com>
2018-04-17 16:13:01 +0200

    Removed irrelevant implement

--- test/gluon/TestGraphClient.ts
+++ test/gluon/TestGraphClient.ts
@@ -1,8 +1,4 @@
-import {
-    GraphClient,
-} from "@atomist/automation-client/spi/graph/GraphClient";
-
-export class TestGraphClient implements GraphClient {
+export class TestGraphClient {
     public endpoint: string;
     public path: string;
     public query: string;
@@ -22,18 +18,8 @@ export class TestGraphClient implements GraphClient {
         return Promise.resolve(json);
     }
 
-    public executeQuery<T, Q>(query: string, variables?: Q, options?: any): Promise<any> {
-        this.query = query;
-        return Promise.resolve();
-    }
-
     public executeMutationFromFile<T, Q>(path: string, variables?: Q, options?: any, current?: string): Promise<any> {
         this.path = path;
-        return Promise.resolve();
-    }
-
-    public executeMutation<T, Q>(mutation: string, variables?: Q, options?: any): Promise<any> {
-        this.mutation = mutation;
         return Promise.resolve();
     }
 
