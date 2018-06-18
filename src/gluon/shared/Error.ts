f6b6c35582ae49c87d37ad19cc4a1608e33a83d9
kbristow <Kieran.Bristow@absa.co.za>
2018-06-18 15:40:14 +0200

    BitbucketProjectRequested to await/async. Implemented new idea for error responses so that we can raise and report errors in a neat manner.

--- src/gluon/shared/Error.ts
+++ src/gluon/shared/Error.ts
@@ -1,10 +1,31 @@
 import {
+    HandlerContext,
     HandlerResult,
     logger,
-    success} from "@atomist/automation-client";
+    success,
+} from "@atomist/automation-client";
+import {Destination} from "@atomist/automation-client/spi/message/MessageClient";
 
 export function logErrorAndReturnSuccess(method, error): HandlerResult {
     logger.info(`Don't display the error - ${method} already handles it.`);
     logger.error(error);
     return success();
+}
+
+export async function handleQMError(ctx: HandlerContext, destinations: Destination|Destination[], error: any) {
+    logger.error("Trying to handle QM error.");
+    if (error instanceof Error) {
+        logger.error("Error is not of QMError type. Letting error bubble up.");
+        throw error;
+    } else if (error instanceof QMError) {
+        logger.error(`Error is of QMError type. Error: ${error.message}`);
+
+        return await ctx.messageClient.send(`❗${error.message}`, destinations);
+    }
+    logger.error("Unknown error type. Rethrowing error.");
+    throw new Error(error);
+}
+
+export class QMError extends Error {
+    // Nothing special
 }
