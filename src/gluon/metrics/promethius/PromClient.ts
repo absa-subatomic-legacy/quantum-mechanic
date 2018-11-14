a539abe2dd5b54ea5d31b80959754d3ab6621acc
Ande De Jager <andre@Andes-MacBook-Pro.local>
2018-11-14 17:41:14 +0200

    Removed using

--- src/gluon/metrics/promethius/PromClient.ts
+++ src/gluon/metrics/promethius/PromClient.ts
@@ -1,5 +1,5 @@
 import {logger} from "@atomist/automation-client";
-import {ListTeamMembers} from "../../commands/team/ListTeamMembers";
+
 export class PromethiusClient  {
 
     public static client: any;
