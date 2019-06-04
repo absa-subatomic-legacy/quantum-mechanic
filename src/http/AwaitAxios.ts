703a65acdb395bd7417f50667678aaabe0d11638
Kieran Bristow <kieran@bristow.za.net>
2019-06-04 15:53:44 +0200

    Added sanitizing to the axios responses to ensure there is always a response status. (#914)

--- src/http/AwaitAxios.ts
+++ src/http/AwaitAxios.ts
@@ -23,42 +23,57 @@ export class AwaitAxios {
 
     public async post(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse> {
         try {
-            return await this.axiosInstance.post(url, data, config);
+            return this.sanitizeResponse(await this.axiosInstance.post(url, data, config));
         } catch (error) {
-            return error.response;
+            return this.sanitizeResponse(error.response);
         }
     }
 
     public async put(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse> {
         try {
-            return await this.axiosInstance.put(url, data, config);
+            return this.sanitizeResponse(await this.axiosInstance.put(url, data, config));
         } catch (error) {
-            return error.response;
+            return this.sanitizeResponse(error.response);
         }
     }
 
     public async get(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse> {
         try {
-            return await this.axiosInstance.get(url, config);
+            return this.sanitizeResponse(await this.axiosInstance.get(url, config));
         } catch (error) {
-            return error.response;
+            return this.sanitizeResponse(error.response);
         }
     }
 
     public async delete(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse> {
         try {
-            return await this.axiosInstance.delete(url, config);
+            return this.sanitizeResponse(await this.axiosInstance.delete(url, config));
         } catch (error) {
-            return error.response;
+            return this.sanitizeResponse(error.response);
         }
     }
 
     public async patch(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse> {
         try {
-            return await this.axiosInstance.patch(url, data, config);
+            return this.sanitizeResponse(await this.axiosInstance.patch(url, data, config));
         } catch (error) {
-            return error.response;
+            return this.sanitizeResponse(error.response);
         }
+    }
+
+    public sanitizeResponse(httpResponse: any) {
+        let httpResponseResult = httpResponse;
+        if (httpResponseResult === undefined) {
+            httpResponseResult = {
+                status: 503,
+                data: {message: "HTTP request returned an undefined result."},
+            };
+        }
+        if (httpResponseResult.status === undefined) {
+            httpResponseResult.status = 503;
+        }
+
+        return httpResponseResult;
     }
 
 }
